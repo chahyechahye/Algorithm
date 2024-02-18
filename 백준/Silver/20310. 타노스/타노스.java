f5160fda18534b76aea80d397d00870682d4e3cb
chahyechahye <122500573+chahyechahye@users.noreply.github.com>
2024-02-18 23:34:24 +0900

    [Silver III] Title: 타노스, Time: 76 ms, Memory: 11512 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -14,32 +14,61 @@ public class Main {
         // S'로 가능한 문자열 중 사전 순 가장 빠른 것 구하기
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
 
-        Queue<Integer> one = new LinkedList<>();
-        Queue<Integer> zero = new LinkedList<>();
-        String ans = "";
+        int arr[] = new int[str.length()];
+        int one = 0;
+        int zero = 0;
 
+        // one zero 갯수 세기
+        // arr에 값 넣어두기
         for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i)-'0' == 1) {
-                one.add(str.charAt(i) - '0');
+            if(str.charAt(i) - '0' == 1) {
+                one++;
             } else {
-                zero.add(str.charAt(i) - '0');
+                zero++;
+            }
+            arr[i] = str.charAt(i) - '0';
+        }
+
+        // 절반씩 없애줄 예정이므로 1/2 값 설정
+        one = one/2;
+        zero = zero/2;
+
+        // 앞에서부터 1인 경우 2로 치환
+        // one의 갯수가 0이 될때까지 진행
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) - '0' == 1) {
+                arr[i] = 2;
+                one--;
+            }
+            if(one == 0) {
+                break;
             }
         }
 
-        int oneSize = one.size();
-        int zeroSize = zero.size();
-
-        for (int i = 0; i < zeroSize/2; i++) {
-            ans += "0";
+        // 앞에서부터 0인 경우 2로 치환
+        // zero의 갯수가 0이 될때까지 진행
+        for (int i = str.length() - 1 ; i >= 0; i--) {
+            if(str.charAt(i) - '0' == 0) {
+                arr[i] = 2;
+                zero--;
+            }
+            if(zero == 0) {
+                break;
+            }
         }
-        for (int i = 0; i < oneSize/2; i++) {
-            ans += "1";
+
+        // arr[i]가 2가 아닐때만 sb에 더해줌
+        for (int i = 0; i < str.length(); i++) {
+            if(arr[i] != 2) {
+                sb.append(arr[i]);
+            }
         }
 
-        System.out.println(ans);
+        System.out.println(sb);
 
     }
 
